c926b0a47c0db719c3a3d028aa346d3a267c1805
Song-Sungyoub <ssyoub0327@daum.net>
2021-10-13 13:52:47 +0000

    design new

--- web/login/index.php
+++ web/login/index.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>오!수병 | Login</title>
     <style>
         body{
@@ -52,7 +53,7 @@
 		<div id="yellow-tab">
 			<!-- 로그인 창 -->
 			<div style="position:absolute;">
-				<form action="./login_action.php" method="post">
+				<form action="/login/login_action.php" method="post">
 					<div>
 						<input type="text" name="" id="">
 						<input type="password" name="" id="">
--- web/ohsailor/index.php
+++ web/ohsailor/index.php
@@ -10,52 +10,131 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>오!수병 | 메인</title>
 </head>
 <body>
-    
-    <!--게시판 부분 미들-->
-    <div style="width:60%;padding-left:20%">
-        <div style="float:left;width:100%;">
-            <div style="float:left;width:30px;">
-                번호
-            </div>
-            <div style="float:left;padding-left:15px;">
-                제목
-            </div>
-            <div style="float:right;width:100px;text-align:center;">
-                작성자
-            </div>
-			<div style="float:right;width:100px;text-align:center;">
-				날짜
-			</div>
-        </div><hr>
-        <div style="float:left;width:100%;">
-            <div style="float:left;width:30px;text-align:center;">
-                1
-            </div>
-            <div style="float:left;padding-left:15px;">
-                오늘 급식 최고!
-            </div>
-			<div style="float:right;width:100px;text-align:center;">
-				익명
-			</div>
-            <div style="float:right;width:100px;text-align:center;">
-                2021.10.09.
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+            <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+            </form>
+
+            <div class="text-end">
+            <button type="button" class="btn btn-outline-light me-2">Login</button>
+            <button type="button" class="btn btn-warning">Sign-up</button>
             </div>
         </div>
-		<div style="float:left;width:100%;">
-            <div style="float:left;width:30px;text-align:center;">
-                2
-            </div>
-            <div style="float:left;padding-left:15px;">
-                미역국에 조미료 줄여주세요.
-            </div>
-            <div style="float:right;width:100px;text-align:center;">
-                익명
-            </div>
-            <div style="float:right;width:100px;text-align:center;">
-                2021.10.09.
+        </div>
+    </header>
+    <!--게시판 부분 미들-->
+    <div class="container">
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+      <span class="fs-4">Sidebar</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link active" aria-current="page">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
+          Orders
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
+          Products
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
+          Customers
+        </a>
+      </li>
+    </ul>
+    <hr>
+    <div class="dropdown">
+      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+        <strong>mdo</strong>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+        <li><a class="dropdown-item" href="#">New project...</a></li>
+        <li><a class="dropdown-item" href="#">Settings</a></li>
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Sign out</a></li>
+      </ul>
+    </div>
+  </div>
+        <div class="d-flex flex-wrap justify-content-center py-3 mb-4">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">번호</th>
+                        <th scope="col">제목</th>
+                        <th scope="col">작성자</th>
+                        <th scope="col">날짜</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr style="cursor:pointer;" onclick="">
+                        <th scope="row">1</th>
+                        <td>오늘 급식 최고!</td>
+                        <td>익명</td>
+                        <td>2021.10.09.</td>
+                    </tr>
+                    <tr style="cursor:pointer;" onclick="">
+                        <th scope="row">2</th>
+                        <td>미역국에 조미료 줄여주세요.</td>
+                        <td>익명</td>
+                        <td>2021.10.09.</td>
+                    </tr>
+                </tbody>
+            </table>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item disabled">
+                    <a class="page-link">Previous</a>
+                    </li>
+                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="container" style="width:100%;">
+                <button type="button" class="btn btn-outline-secondary" onclick="" style="float:right;">글쓰기</button>
             </div>
         </div>
     </div>
